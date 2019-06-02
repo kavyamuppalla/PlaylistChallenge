7235114af62480043dc84da24e5352083220282e
Kavya Muppalla <kavyamuppalla@zipcoders-MBP-2.fios-router.home>
2019-06-01 20:22:07 -0400

    Playlist Challenge

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,21 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int count = 0;
+        int upcount = 0;
+        //int downcount = 0;
+
+        for(int i = 0; i < playList.length; i++) {
+             if(selection.equals(playList[i])) {
+                 count = Math.abs(startIndex - i);
+                 upcount = Math.abs(startIndex + playList.length - i);
+            }
+        }
+        return Math.min(count,upcount);
     }
 }
